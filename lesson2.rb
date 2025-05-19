5a6ad214db013f767e5f41961b92855bc1734461
shuri <inoseshuri@icloud.com>
2025-05-19 21:56:29 +0900

    5/19

---
+++ lesson2.rb
@@ -0,0 +1,36 @@
+#問１
+puts 2 + 3
+
+#問２
+puts 2 * 2 * 3.14 
+
+#問３
+puts "Ruby"
+
+#問4
+puts "abc" + "123"
+
+#問5
+puts "123".to_i + "456".to_i
+
+#問6
+coffer_price = 300
+shot_price = 100
+puts "コーヒー：#{coffer_price}円"
+puts "合計：#{coffer_price + shot_price * 2}円"
+
+#問7
+coffer_price = 400
+shot_price = 100
+puts "コーヒー：#{coffer_price}円"
+puts "合計：#{coffer_price + shot_price * 2}円"
+
+#問8
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
+
+#問9
+total = 300 * 2
+puts total
