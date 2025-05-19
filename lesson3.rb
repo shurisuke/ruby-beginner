3327b80b6978a7e7aee5d9c087a73bf79cf45129
shuri <inoseshuri@icloud.com>
2025-05-19 22:36:08 +0900

    5/19

--- lesson3.rb
+++ lesson3.rb
@@ -1,25 +1,61 @@
 #問1
-
+a = 2
+puts a < 365
 
 #問2
-
+a = 2
+puts a == 1 + 1
 
 #問3
-
+season = "春"
+if season != "夏"
+    puts "あんまん食べたい"
+end
 
 #問4
-
+season = "夏"
+if season == "夏"
+    puts "かき氷食べたい"
+    puts "麦茶飲みたい"
+end
 
 #問5
-
+wallet = 100
+if wallet >= 120 
+    puts "ジュース買おう"
+else 
+    puts "お金じゃ買えない幸せがたくさんあるんだ"
+end
 
 #問6
-
+x = 200
+if x <= 0 || x >= 100
+    puts "範囲外です"
+end
 
 #問7
-
+x = 0
+y = -1
+z = 1
+if x > 0 || y > 0 || z > 0
+    puts "正の数です"
+end
 
 #問8
-
+season = "春"
+case season
+when "春"
+    puts "アイスを買って行こう！"
+when "夏"
+    puts "かき氷買ってこう！"
+else
+    puts "あんまん買ってこう！"
+end
 
 #問9
+2.times do
+    puts "カフェラテ"
+    puts "モカ"
+    puts "モカ"
+end
+puts "フラペチーノ"
